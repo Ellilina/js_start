42720ee020f7c0c1b2d6a5866f10d25547aad7a1
Ellina <ellina485@gmail.com>
2024-09-23 20:31:07 +0300

    мозданы файлы

---
+++ scrpt.js
@@ -0,0 +1,67 @@
+const userInput1 = prompt('введите число')
+// const userInput2 = +prompt("Введите еще число")
+
+// function isPositive(number) {
+//     alert(`Вход ${number}, тип ${typeof number}`)
+
+//     if ((typeof number !== 'numer') || (!number && number !== 0)) {
+//     return "Введено некорректное число"
+//     }
+
+//     if (number > 0) {
+//         return "ПОложительное"
+//     } else if (number < 0) {
+//         return "Отрицательное"
+//     } else if (number === 0) {
+//         return "Ноль"
+//     } else {
+//         return "Введено некорректное число"
+//     }
+// }
+
+// // function isValidAge(age) {
+//     if(age <= 120 && age >= 0) {
+//         return "Возраст корректный"
+//     }
+//     return "Возраст не орректный"
+// }
+
+// function getGreater(num1, num2) {
+//     return num1 >= num2 ? num1 : num2
+// }
+
+// function isKratno5(number) {
+//     // return number % 5 ? "Не кратно" : "Кратно"
+//     if (number % 5 !== 0) {
+//         return "Не кратно"
+//     } else if (number % 5 === 0) {
+//         return "Кратно"
+//     }
+// }
+
+// function isEarth(planet) {
+//     planet = planet.toLowerCase()
+
+//     return `Привет, ${planet === 'земля' ? 'землянин' : 'иноплонетянин'}!`
+// }
+
+// function calculate(num1, num2, operator)
+// switch(operator) {
+//     case '+' :
+//         return num1 + num2
+//     case '-' :
+//       return  num1 - num2
+//     case '*' :
+//       return  num1 * num2
+//     case "/" :
+//        return num1 / num2 
+
+//     default:
+//         return "Будьте внимательнее!"
+// }
+
+function mod(num1, num2) {
+    
+}
+
+alert(isEarth(userInput1))
